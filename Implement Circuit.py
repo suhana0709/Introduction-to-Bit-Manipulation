3ecd198f2774c03a39a52934c0b7baee1de7df75
suhana0709 <farjanajaman1975@gmail.com>
2025-11-07 09:05:58 -0600

    Add files via upload

---
+++ Implement Circuit.py
@@ -0,0 +1,10 @@
+# Inputs
+A = int(input("Enter A (0 or 1): "))
+B = int(input("Enter B (0 or 1): "))
+C = int(input("Enter C (0 or 1): "))
+
+# Logic
+Q = B and (A or C)
+
+# Output
+print("Output Q =", int(Q))
