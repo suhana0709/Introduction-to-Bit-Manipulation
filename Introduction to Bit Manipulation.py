610864d1789d25da3ca07ff80d010e698987410d
suhana0709 <farjanajaman1975@gmail.com>
2025-11-07 06:36:22 -0600

    Add files via upload

---
+++ Introduction to Bit Manipulation.py
@@ -0,0 +1,12 @@
+#Implement Bitwise
+
+num1 = 10
+num2 = 4
+
+print("num1 and num2 are:", num1, num2)
+print("num1 | num2 are:", num1 | num2)  #OR
+print("num1 ^ num2 are:", num1^num2)    #XOR
+print("num1 << num2 are:", num1 << num2)    # left bit shifting
+print("num1 >> num2 are:", num1>>num2)         # right bit shifting
+print("~num1 : ",~num1)      #not
+print("~num2 : ",~num2)
