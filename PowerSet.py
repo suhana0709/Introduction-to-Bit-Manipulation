cbba33403f091b4ef57e8dac39fc1c4087dd6817
suhana0709 <farjanajaman1975@gmail.com>
2025-11-28 07:34:22 -0600

    Add files via upload

---
+++ PowerSet.py
@@ -0,0 +1,13 @@
+import math
+
+def PrintPowerSet(set, SetSize):
+    PowerSetSize = int(math.pow(2, SetSize))
+    for outer in range(PowerSetSize):
+        for inner in range(SetSize):
+            if (outer & (1 << inner)) > 0:
+                print(set[inner], end=" ")
+        print()
+
+size = int(input("Enter the array size: "))
+set = [int(input("Enter the element: ")) for _ in range (size)]
+print(PrintPowerSet(set, len(set)))
